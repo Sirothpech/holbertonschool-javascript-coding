74a892f8ac175bda0ea78d2be0f8cad26893bb7e
Sirothpech <christophe.ngan@gmail.com>
2023-09-20 14:27:10 +0200

    Use the variable const to the argv2

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
