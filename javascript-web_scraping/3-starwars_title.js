4b40691f8e9a5bdc57c551e79a66b051d8875682
Sirothpech <christophe.ngan@gmail.com>
2023-09-20 14:18:53 +0200

    Add: task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error.message);
+  } else {
+    if (response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+    }
+  }
+});
