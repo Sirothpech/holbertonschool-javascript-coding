57bcd89a49e26413fc594f22b782a6575877a6ad
Sirothpech <christophe.ngan@gmail.com>
2023-09-20 13:29:49 +0200

    Add: task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs'); // Importer le module fs (file system)
+
+const filePath = process.argv[2]; // Spécifier le chemin du fichier à lire
+const text = process.argv[3]; // Spécifier le texte à écrire
+
+fs.writeFile(filePath, text, 'utf-8', (error) => {
+  if (error) {
+    console.error(error); // Afficher l'erreur si elle se produit
+  } else {
+    console.log("Writing success"); // Afficher le contenu du fichier lu
+  }
+});
