6ea72c67dfb21c921152b1ed7b14f491ee8543b4
Sirothpech <christophe.ngan@gmail.com>
2023-10-10 11:14:02 +0200

    Fix the sentence hello holberton school

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -6,7 +6,7 @@ const port = 1245;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School! ');
+  res.end('Hello Holberton School!');
 });
 
 server.listen(port, hostname, () => {
